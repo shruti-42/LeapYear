ddac37a18bcdc92f9d908c06a6fd34ce60a720cd
Your Name <you@example.com>
2019-11-07 16:34:42 +0530

    FEAT:Year input field added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "./App.css";
 
 class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <form className="leapYear">
+          <p className="leapHeading">Leap Years</p>
+          <span>Input Year: </span>
+          <input type="text" id="year" />
+        </form>
+      </div>
+    );
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('should render App without throwing an error', () => {
+  expect(shallow(<App />).find('form.leapYear').exists()).toBe(true)
+});
+
+it('renders a input field', () => {
+  expect(shallow(<App />).find('#year').length).toEqual(1)
+ });
